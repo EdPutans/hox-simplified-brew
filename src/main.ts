2a855ccbea8ad6226aae720183f38a4a9c5aa18b
Ed 'Catboy' Putans <edputans@gmail.com>
2022-07-06 15:38:05 +0100

    i can render multiple! yay!

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,86 @@
 import './style.css';
 
+const testBreweries = [
+  {
+    "id": "banjo-brewing-fayetteville",
+    "name": "Banjo Brewing",
+    "brewery_type": "planning",
+    "street": null,
+    "address_2": null,
+    "address_3": null,
+    "city": "Fayetteville",
+    "state": "West Virginia",
+    "county_province": null,
+    "postal_code": "25840",
+    "country": "United States",
+    "longitude": null,
+    "latitude": null,
+    "phone": "3042164231",
+    "website_url": null,
+    "updated_at": "2021-10-23T02:24:55.243Z",
+    "created_at": "2021-10-23T02:24:55.243Z"
+  },
+  {
+    "id": "barrel-brothers-brewing-company-windsor",
+    "name": "Barrel Brothers Brewing Company",
+    "brewery_type": "micro",
+    "street": "399 Business Park Ct Ste 506",
+    "address_2": null,
+    "address_3": null,
+    "city": "Windsor",
+    "state": "California",
+    "county_province": null,
+    "postal_code": "95492-6652",
+    "country": "United States",
+    "longitude": null,
+    "latitude": null,
+    "phone": "7076969487",
+    "website_url": "http://www.barrelbrothersbrewing.com",
+    "updated_at": "2021-10-23T02:24:55.243Z",
+    "created_at": "2021-10-23T02:24:55.243Z"
+  },
+  {
+    "id": "bay-brewing-company-miami",
+    "name": "Bay Brewing Company",
+    "brewery_type": "planning",
+    "street": null,
+    "address_2": null,
+    "address_3": null,
+    "city": "Miami",
+    "state": "Florida",
+    "county_province": null,
+    "postal_code": "33130-3488",
+    "country": "United States",
+    "longitude": null,
+    "latitude": null,
+    "phone": "18134763767",
+    "website_url": null,
+    "updated_at": "2021-10-23T02:24:55.243Z",
+    "created_at": "2021-10-23T02:24:55.243Z"
+  }
+]
+
+// const testBrewery = {
+//   id: "bay-brewing-company-miami",
+//   name: "Bay Brewing Company",
+//   brewery_type: "planning",
+//   street: null,
+//   address_2: null,
+//   address_3: null,
+//   city: "Miami",
+//   state: "Florida",
+//   county_province: null,
+//   postal_code: "33130-3488",
+//   country: "United States",
+//   longitude: null,
+//   latitude: null,
+//   phone: "18134763767",
+//   website_url: null,
+//   updated_at: "2021-1023T02:55.243Z",
+//   created_at: "2021-1023T0:24:55.243Z"
+// };
+
+
 const main = document.querySelector('main');
 
 const article = document.createElement('article');
@@ -8,31 +89,35 @@ if (main) {
   main.append(article);
 }
 
-const ulEl = document.createElement('ul');
-ulEl.className = 'breweries-list';
+for (let testBrewery of testBreweries) {
 
-article.append(ulEl);
+  const ulEl = document.createElement('ul');
+  ulEl.className = 'breweries-list';
 
-const liEl = document.createElement('li');
-ulEl.append(liEl);
+  article.append(ulEl);
 
-const h2El = document.createElement('h2');
-h2El.textContent = 'Snow Belt Brew'
-liEl.append(h2El);
+  const liEl = document.createElement('li');
+  ulEl.append(liEl);
 
-const divEl = document.createElement('div');
-divEl.className = 'type';
-divEl.textContent = 'micro'
-liEl.appendChild(divEl);
+  const h2El = document.createElement('h2');
+  h2El.textContent = testBrewery.name;
+  liEl.append(h2El);
 
-const sectionEl = document.createElement('section');
-sectionEl.className = 'address';
+  const divEl = document.createElement('div');
+  divEl.className = 'type';
+  divEl.textContent = 'micro'
+  liEl.appendChild(divEl);
 
-const h3 = document.createElement('h3');
-h3.textContent = 'Address:'
-const p = document.createElement('p');
-p.textContent = '9511 Kile Rd'
-const pp = document.createElement('p');
+  const sectionEl = document.createElement('section');
+  sectionEl.className = 'address';
 
-sectionEl.append(h3, p, pp)
-liEl.appendChild(sectionEl);
+  const h3 = document.createElement('h3');
+  h3.textContent = 'Address:'
+  const p = document.createElement('p');
+  p.textContent = testBrewery.street || "No address for this boi"
+  const pp = document.createElement('p');
+
+  sectionEl.append(h3, p, pp)
+  liEl.appendChild(sectionEl);
+
+}
